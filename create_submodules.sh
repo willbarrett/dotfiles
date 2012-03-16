09f0210c949a33c90828d689ad8eded3e8972b6e
Will Barrett <william.barrett@tallgreentree.com>
2012-03-16 13:06:16 -0500

    Fixing some sillynes

---
+++ create_submodules.sh
@@ -0,0 +1,7 @@
+git submodule add https://github.com/kien/ctrlp.vim.git .vim/bundle/ctrlp.vim
+git submodule add https://github.com/scrooloose/syntastic.git .vim/bundle/syntastic
+git submodule add git://github.com/altercation/vim-colors-solarized.git .vim/bundle/vim-colors-solarized
+git submodule add https://github.com/tpope/vim-commentary.git .vim/bundle/vim-commentary
+git submodule add https://github.com/tpope/vim-fugitive.git .vim/bundle/vim-fugitive
+git submodule add https://github.com/tpope/vim-rails.git .vim/bundle/vim-rails
+git submodule add https://github.com/vim-ruby/vim-ruby.git .vim/bundle/vim-ruby
